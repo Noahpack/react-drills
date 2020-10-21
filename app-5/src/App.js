3fb97f8bc0e53be9c416997437badc6a9c74198a
Noah Pack <noahpack@noahs-mbp.lan>
2020-10-21 17:30:23 -0600

    Finished the first six apps

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Image from './image'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render() {
+    return (
+      <div className="App">
+        <Image url={"https://www.businessupturn.com/wp-content/uploads/2020/09/Untitled-design-24-3.jpg"}/>
+      </div>
+    )
+  }
+
 }
 
 export default App;
